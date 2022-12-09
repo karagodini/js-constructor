12ae93ec01daeb8cfc1400d21f75194902120211
vipsmmtime <vipsmmtime@yandex.ru>
2022-12-10 01:52:37 +0300

    js

---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+    {type: 'title', value: 'Hello Wolrd from js'},
+    {type: 'text', value: 'here we go with some text'},
+    {type: 'columns', value: [
+        '11111111',
+        '22222222',
+        '33333333'
+    ]},
+    {type: 'image', value: 'assets/image.png'}
+]
